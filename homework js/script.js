3b44e196b2361e72470edd502d861607a484a8cd
arshakpoghosyan <poghosyanal28@gmail.com>
2021-12-03 17:36:06 +0400

    .

---
+++ homework js/script.js
@@ -0,0 +1,12 @@
+
+let age = prompt('How old are you?');
+if (age>=18 && age<21) {
+alert("Nearly, but not yet bro");
+}
+else if (age>=21) {
+    alert("Welcome to the club");
+}
+     
+else {
+alert("hm, come back with dad");
+}
