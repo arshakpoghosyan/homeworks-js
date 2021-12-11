29e4878d9758d444a2237778746a3e3bd5f9ad6c
arshakpoghosyan <poghosyanal28@gmail.com>
2021-12-11 15:20:23 +0400

    .

--- every/script.js
+++ every/script.js
@@ -1,18 +1,19 @@
 function every(array, func) {
-    for(let i = 0; i < array.length; i++) {
-      if (func(array[i])) {
-          return true;
-        }
-      else
-          return false;
-      }
+  for (let i = 0; i < array.length; i++) {    
+    if (!func(array[i])) {
+      return false;
+    } else if ((i == array.length-1) && func(array[i])) {
+      return true;
+    }
   }
-  
-  function even(num) {
-      if (num % 2 == 0 && num % 2 !=1) {
-        return true;
-    }
-      else
-        return false;
-    }
-  alert(every([1, 2, 6, 8], even));
+}
+
+function even(num) {
+  if (num % 2 != 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+let arr = [2, 1, 4, 8];
+alert(every(arr, even));
